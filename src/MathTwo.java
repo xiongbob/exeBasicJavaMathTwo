7cef5da58004f9877bdc985123506f51c00af76c
xiongbob <xiong.bob@gmail.com>
2019-06-12 11:42:46 -0400

    whatever-commit-MathTwo

---
+++ src/MathTwo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class MathTwo {
+    public static void main(String args[]) {
+        try {
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+
+        System.out.println("My Number " + mynumber);
+        System.out.println("My other number " + myothernumber);
+        } catch(Exception e) {}
+    }
+}
